18639f35a1ffa5a3c259302ecd6c3d058c9a9e18
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-10-11 17:43:22 +0200

    setup project boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <p>React App</p>
+  </div>
+);
+
+export default App;
---
+++ webpack/common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../', 'src', 'index.js'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif|jpeg)$/,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      inject: 'body',
+      title: 'Weather App',
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon.png',
+    }),
+  ],
+};
